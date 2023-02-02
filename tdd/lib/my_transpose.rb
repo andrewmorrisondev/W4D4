f96b94e5f0cb5a5c56c39137bbb4022b58c998e0
Fahim Khan <fk652@nyu.edu>
2023-02-02 17:04:33 -0500

    add: my_transpose

---
+++ tdd/lib/my_transpose.rb
@@ -0,0 +1,14 @@
+class Array
+  def my_transpose
+    transposed_array = []
+    self.each_with_index do |e1, i1|
+
+    end
+    (0..self.length-1).each do |col_idx|
+      
+      (0..self.length-1).each do |row_idx|
+
+      end
+    end
+  end
+end
---
+++ tdd/spec/my_transpose_spec.rb
@@ -0,0 +1,67 @@
+require "rspec"
+require "my_transpose"
+
+describe Array do
+  describe "#my_transpose" do
+    subject(:array) { 
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ]
+    }
+
+    let(:transposed_array) {
+      [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ]
+    }
+
+    let(:array2) {
+      [
+        [0, 3, 6, 4],
+        [1, 4, 7, 10],
+        [2, 5, 8, 3],
+        [1, 2, 3, 4]
+      ]
+    }
+
+    let(:transposed_array2) {
+      [
+        [0, 1, 2, 1],
+        [3, 4, 5, 2],
+        [6, 7, 8, 3],
+        [4, 10, 3, 4]
+      ]
+    }
+
+    it "should return a new 2d array" do
+      expect(array.my_transpose).to_not be(array)
+    end
+
+    it "should have the same number of rows as the original" do
+      expect(array.my_transpose.size).to eq(3)
+    end
+
+    it "should have the same number of columns as the original" do
+      expect(array.my_transpose).to all(should have(3).items)
+    end
+
+    it "should not change position [0,0], [1, 1], or [2,2]" do
+      expect(array.my_transpose[0][0]).to eq(0)
+      expect(array.my_transpose[1][1]).to eq(4)
+      expect(array.my_transpose[2][2]).to eq(8)
+    end
+
+    it "should correctly flip the rows and columns" do
+      expect(array.my_transpose).to eq(transposed_array)
+    end
+
+    it "should work on a square array of any size" do
+      expect(array2.my_transpose).to eq(transposed_array2)
+    end
+
+  end
+end
